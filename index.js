0259382af2715a916ba809cb9a0508843e264317
joseucarvajal <joseucarvajal@gmail.com>
2017-07-30 13:45:50 -0500

    change telegram api client library to: node-telegram-bot-api

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
 'use strict'
 
-const Telegram = require('telegram-node-bot');
-const TextCommand = Telegram.TextCommand;
 
-const tg = new Telegram.Telegram('447133612:AAG96SqODQfDB9sXv9YB9GLdWEg15BxekPQ', {
-  webhook: {
-    url: 'https://evening-headland-56271.herokuapp.com/node-bot-webhook',
-    port: process.env.PORT || 5000
-  }
+const TelegramBot = require('node-telegram-bot-api');
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '447133612:AAG96SqODQfDB9sXv9YB9GLdWEg15BxekPQ';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  console.log('Llega /echo');
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  //bot.sendMessage(chatId, resp);
+  
+  const fromId = msg.from.id;
+
+  let messageOptions = {
+    reply_markup: {
+      resize_keyboard: true,
+      one_time_keyboard: true,
+      keyboard: [
+        ['yes'],
+        ['no']
+      ],
+    },
+  };
+
+  bot.sendMessage(chatId, 'Selecciona una opción', messageOptions);
+
 });
 
-class LabController extends Telegram.TelegramBaseController {
 
-  /**
-   * @param {Scope} $
-   */
-  n1Handler($) {
-    $.setUserSession('n1', this.getNumericValueFromCommand($.message.text));
-    $.sendMessage('Muy bien, ahora ingresa el numero 2 con /n2');
-  }
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
 
-  n2Handler($) {
-    $.setUserSession('n2', this.getNumericValueFromCommand($.message.text));
-    $.sendMessage('Muy bien, ahora ingresa el comando /result');
-  }
-
-  resultHandler($) {
-    $.getUserSession('n1').then(n1 => {
-      $.getUserSession('n2').then(n2 => {
-        const result = parseInt(n1) + parseInt(n2);
-        $.sendMessage("La suma es: " + result);
-      });
-    });
-  }
-
-  get routes() {
-    return {
-      'pingCommand': 'pingHandler',
-      'n1Command': 'n1Handler',
-      'n2Command': 'n2Handler',
-      'resultCommand': 'resultHandler'
-    }
-  }
-
-  getNumericValueFromCommand(comandValue) {
-    return comandValue.split(' ').slice(1).join(' ')
-  }
-
-}
-
-class OtherwiseController extends Telegram.TelegramBaseController {
-  handle($) {
-    $.sendMessage('Ingresa alguna de las siguientes opciones: /n1, /n2, /result');
-  }
-}
-
-tg.router
-  .when(new TextCommand('/ping', 'pingCommand'), new LabController())
-  .when(new TextCommand('/n1', 'n1Command'), new LabController())
-  .when(new TextCommand('/n2', 'n2Command'), new LabController())
-  .when(new TextCommand('/result', 'resultCommand'), new LabController())
-  .otherwise(new OtherwiseController());
+  // send a message to the chat acknowledging receipt of their message
+  //bot.sendMessage(chatId, 'Received your message');
+});
