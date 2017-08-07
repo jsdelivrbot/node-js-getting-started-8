6673248bda5a10fc911801e2e000f04a6da035f3
joseucarvajal <joseucarvajal@gmail.com>
2017-08-07 13:42:56 -0500

    attendee-registration wireframing.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ require('./initBot');
 require('./app/index');
 require('./app/student-registration');
 require('./app/atendee-registration');
+require('./app/manage-homeworks');
 
 require('./jobs/notify-grades-job');
