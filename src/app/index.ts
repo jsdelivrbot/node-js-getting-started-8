9e69573a441733b5896319d7dfbaa686f485ede1
Luis Felipe Mejia Castrillon <luisfe_617@outlook.com>
2017-08-07 13:44:30 -0500

    student registration menu

--- src/app/index.ts
+++ src/app/index.ts
@@ -10,38 +10,68 @@ import { ReplyKeyboardMarkup } from "../bot/ReplyKeyboardMarkup";
 import { SendMessageOptions } from "../bot/SendMessageOptions";
 import { Chat } from "../bot/Chat";
 import { Chat as ChatModel } from '../core/contracts';
+import { KeyboardButton } from "../bot/KeyboardButton";
 
-namespace reply_markups {
+import { Messages as StudentMessages } from './student-registration';
 
-    export namespace index {
+export namespace ReplyMarkups {
 
-        const keyBoardOptions = {
-            Estudiante: 'Estudiante',
-            Profesor: 'Profesor',
+    export namespace Index {
+
+        export const Estudiante: KeyboardButton = {
+            text: 'Estudiante'
         };
 
-        export const reply_markup = {
+        export const Profesor: KeyboardButton = {
+            text: 'Profesor'
+        };
+
+        export const keyboard: Array<Array<KeyboardButton>> = [
+            [Estudiante],
+            [Profesor]
+        ];
+
+        export const start_markup = {
             resize_keyboard: true,
             one_time_keyboard: true,
-            keyboard: [
-                [keyBoardOptions.Estudiante, keyBoardOptions.Profesor]
-            ],
+            keyboard: keyboard,
         } as ReplyKeyboardMarkup;
     }
 }
 
+export namespace Messages {
+
+    export namespace Index {
+
+        const messageOptions = {
+            parse_mode: 'HTML',
+            reply_markup: ReplyMarkups.Index.start_markup
+        } as SendMessageOptions;
+
+        export const sendStartMessage = (msg: Message) => {
+            bot.sendMessage(
+                msg.chat.id,
+                `Hola <b>${msg.from.first_name}</b>, escoja su perfil`,
+                messageOptions
+            );
+        };
+    }
+}
+
 bot.onText(/^\/start$/, (msg: Message, match: any) => {
-
-    let messageOptions = {
-        parse_mode: 'HTML',
-        reply_markup: reply_markups.index.reply_markup
-    } as SendMessageOptions;
-
     let chat: ChatModel | null = Data.Chats.getById('ad');
+    Messages.Index.sendStartMessage(msg);
+});
 
-    bot.sendMessage(
-        msg.chat.id,
-        `Hola <b>${msg.from.first_name}</b>, escoje tu categoria`,
-        messageOptions
-    );
+bot.on('message', (msg: Message) => {
+
+    if (!msg.text) {
+        return;
+    }
+
+    if (msg.text.indexOf(ReplyMarkups.Index.Estudiante.text) === 0) {
+        StudentMessages.StudentRegistration.sendStudentMessage(msg)
+    } if (msg.text.indexOf(ReplyMarkups.Index.Profesor.text) === 0) {
+        console.log("Profesor profile")
+    }
 });
--- src/app/student-registration.ts
+++ src/app/student-registration.ts
@@ -1,20 +1,62 @@
+import {
+    bot,
+    dataBase
+} from '../initBot';
+
+import { Chat } from "../bot/Chat";
+import { Chat as ChatModel } from '../core/contracts';
+import { Message } from "../bot/Message";
+import { SendMessageOptions } from "../bot/SendMessageOptions";
+import { KeyboardButton } from "../bot/KeyboardButton";
 import { ReplyKeyboardMarkup } from "../bot/ReplyKeyboardMarkup";
 
-namespace reply_markups {
+import * as Data from '../data';
+
+export namespace ReplyMarkups {
 
     export namespace StudentRegistration {
 
-        const keyBoardOptions = {
-            Estudiante: 'Estudiante',
-            Profesor: 'Profesor',
+        export const MisDatos: KeyboardButton = {
+            text: 'Mis datos'
         };
 
-        export const reply_markup = {
+        export const Asistencia: KeyboardButton = {
+            text: 'Registrar asistencia'
+        };
+
+        export const Volver: KeyboardButton = {
+            text: 'Volver'
+        };
+
+        const keyboard: Array<Array<KeyboardButton>> = [
+            [MisDatos],
+            [Asistencia],
+            [Volver]
+        ];
+
+        export const student_markups = {
             resize_keyboard: true,
             one_time_keyboard: true,
-            keyboard: [
-                [keyBoardOptions.Estudiante, keyBoardOptions.Profesor]
-            ],
+            keyboard: keyboard,
         } as ReplyKeyboardMarkup;
+    }
+}
+
+export namespace Messages {
+
+    export namespace StudentRegistration {
+
+        const messageOptions = {
+            parse_mode: 'HTML',
+            reply_markup: ReplyMarkups.StudentRegistration.student_markups
+        } as SendMessageOptions;
+
+        export const sendStudentMessage = (msg: Message) => {
+            bot.sendMessage(
+                msg.chat.id,
+                `Hola <b>${msg.from.first_name}</b>, escoja su perfil`,
+                messageOptions
+            );
+        };
     }
 }
